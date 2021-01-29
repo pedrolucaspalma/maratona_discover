5a7482cb1a3de5858b6fec8b78c83e1d2cabfd30
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-01-29 08:35:39 -0300

    organizar funcionalidades iniciais no js

--- scripts.js
+++ scripts.js
@@ -11,3 +11,52 @@ const Modal = {
 
     }
 }
+
+/*Array de objetos*/const transactions = [
+    {
+        id: 1,
+        description: 'Luz',
+        amount: -50000,
+        date: '23/01/2021'
+    },
+    {
+        id: 2,
+        description: 'Website',
+        amount: 500000,
+        date: '23/01/2021'
+    },
+    {
+        id: 1,
+        description: 'Luz',
+        amount: -20000,
+        date: '23/01/2021'
+    },
+]
+
+const Transaction = {
+    incomes(){
+        // somar as entradas
+    },
+    expenses(){
+        // somar as saídas
+    },
+    total(){
+        // entradas - saídas
+    }
+}
+
+/*Objeto*/const DOM = {
+    /*Função*/innerHTMLTransaction(){
+
+            const html = `
+            <tr>
+                <td class="description">Luz</td>
+                <td class="expense">- R$ 500,00</td>
+                <td class="date">23/01/2021</td>
+                <td>
+                    <img src="./assets/minus.svg" alt="Remover transação">
+                </td>
+            </tr> 
+            `
+    }
+}
