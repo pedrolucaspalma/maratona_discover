9d616d2aa92355ce04915b3eaab4aea985b505ff
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-01-29 17:56:07 -0300

    adicionar funcionalidades js

--- scripts.js
+++ scripts.js
@@ -26,8 +26,8 @@ const Modal = {
         date: '23/01/2021'
     },
     {
-        id: 1,
-        description: 'Luz',
+        id: 3,
+        description: 'Internet',
         amount: -20000,
         date: '23/01/2021'
     },
@@ -46,17 +46,49 @@ const Transaction = {
 }
 
 /*Objeto*/const DOM = {
-    /*Função*/innerHTMLTransaction(){
+    transactionsContainer: document.querySelector('#data-table tbody'),
 
-            const html = `
-            <tr>
-                <td class="description">Luz</td>
-                <td class="expense">- R$ 500,00</td>
-                <td class="date">23/01/2021</td>
-                <td>
-                    <img src="./assets/minus.svg" alt="Remover transação">
-                </td>
-            </tr> 
-            `
+    addTransaction(transaction, index){
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+        DOM.transactionsContainer.appendChild(tr)
+    },
+
+    /*Função*/innerHTMLTransaction(transaction){
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+        const amount = Utils.formatCurrency(transaction.amount)
+
+        const html = `
+        <td class="description">${transaction.description}</td>
+        <td class="${CSSclass}">${amount}</td>
+        <td class="date">${transaction.date}</td>
+        <td>
+            <img src="./assets/minus.svg" alt="Remover transação">
+        </td>
+        `
+        return html
     }
 }
+
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : ""
+
+        value = String(value).replace(/\D/g, "")
+
+        value = Number(value) / 100
+
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        })
+
+        return signal + value
+    }
+}
+
+transactions.forEach(function(transaction){
+    DOM.addTransaction(transaction)
+})
